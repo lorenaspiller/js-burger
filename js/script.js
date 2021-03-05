0c4dc6c93c2c40651a23fb313b923b59266133b6
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-05 17:08:30 +0100

    added complete javascript

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
-// 1. aggiungo evento click sul bottone
+var calculate = document.getElementById("calculate");
+var nameInput = document.getElementById("name__input");
+var customIngredient = document.getElementsByClassName("custom-ingredient");
+var total = document.getElementById("total");
+var coupon = document.getElementById('coupon__input');
+// coupons array
+var coupons = ["BestDietEva", "mustard-friendly", "junk101", "cheeseaholic"];
+console.log(coupons);
 
-var calculate = document.getElementById('calculate');
-var nameInput = document.getElementById('name__input')
-
+// 1. adding the event on the button
 calculate.addEventListener("click",
   function() {
     // console.log(calculate);
-    //2. controllo se è stato inserite il nome del burger
+    //2. control on the input name of the burger
     if (nameInput.value != "") {
       // console.log(nameInput.value);
+      // 3. checking which element is selected through the list
+      var sum = 50;
+      for (var i = 0; i < customIngredient.length; i++) {
+        if (customIngredient[i].checked) {
+          sum += parseInt(customIngredient[i].value);
+        }
+      }
+      // 4. adding 20% discount if the user has a coupon
+      if (coupons.includes(coupon.value)) {
+        sum -= sum * 0.2;
+        // 5.bonus - making sure the user cannot use the same coupon twice
+        var indexCoupon = coupons.indexOf(coupon.value);
+        console.log(indexCoupon);
+        coupons.splice(indexCoupon, 1);
+        console.log(coupons);
+      }
+      // 5. printing on screen the total sum
+      total.innerHTML = sum.toFixed(2);
     } else {
       alert("Name your burger first!");
     }
-
   }
 );
